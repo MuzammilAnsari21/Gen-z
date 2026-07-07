6f47f88e7befe882b13b112900ade2e1c7e7a24e
MuzammilAnsari21 <muzammil.splitarts@gmail.com>
2026-07-07 21:58:30 +0500

    navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '@/assets/img/logo.png'
+import logo from '@/assets/img/Logo.png'
 
 export default function Header() {
     return (
